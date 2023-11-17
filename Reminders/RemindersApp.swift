bf5fd144d37da2c7badc00cd50fa8bae9790877f
alessiabarone <alessiabarone1410@gmail.com>
2023-11-17 11:14:58 +0100

    added project
    created the new project

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Alessia Barone on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
